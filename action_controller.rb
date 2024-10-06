714fb29334133371c05403a97814fb6fa0fa1086
elasticspoon <mail@yuribocharov.dev>
2024-10-06 14:46:56 -0400

    feat: rack + router object allocation overhead

--- action_controller.rb
+++ action_controller.rb
@@ -17,7 +17,8 @@ class TestApp < Rails::Application
   config.hosts << 'example.org'
   config.secret_key_base = 'secret_key_base'
 
-  config.logger = Logger.new($stdout)
+  # config.logger = Logger.new($stdout)
+  config.logger = Logger.new(File::NULL)
   Rails.logger  = config.logger
 
   routes.draw do
@@ -39,14 +40,46 @@ require 'rack/test'
 class BugTest < ActiveSupport::TestCase
   include Rack::Test::Methods
 
-  def test_returns_success
-    get '/'
-    assert last_response.ok?
+  def test_allocations_skipping_router
+    repeat_times(100, test_name: 'Skipping rack + router') do
+      TestController.action(:index).call(mock_request)
+    end
+    assert true
+  end
+
+  def test_allocations_skipping_rack
+    repeat_times(100, test_name: 'Skipping rack') do
+      app.routes.call(mock_request)
+    end
+    assert true
+  end
+
+  def test_base_request_allocations
+    repeat_times(100, test_name: 'Full request') do
+      app.call(mock_request)
+    end
+    assert true
   end
 
   private
 
   def app
     Rails.application
+  end
+
+  def repeat_times(times, test_name: nil, &block)
+    allocs = (0..times).map { allocations { block.call } }
+    allocs = allocs.sort
+    puts "#{test_name} allocated #{allocs[allocs.length / 2]} objects".strip
+  end
+
+  def allocations
+    x = GC.stat(:total_allocated_objects)
+    yield
+    GC.stat(:total_allocated_objects) - x
+  end
+
+  def mock_request
+    Rack::MockRequest.env_for('http://example.org', 'HTTP_HOST' => 'example.org')
   end
 end
